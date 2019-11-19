5a9cd044e23ee8d22eef78e8aaff55f8d688e8be
YousefQwasmeh <151037@ppu.edu.ps>
2019-11-19 16:48:59 +0200

    0

--- client/components/chatRoom/index.js
+++ client/components/chatRoom/index.js
@@ -22,33 +22,48 @@ class ChatRoom extends Component {
 
   render() {
     const chatMessages = this.props.chatMessages.map((msgInfo, i) => (
-      <View key={msgInfo.description + i}>
+      <View
+        style={
+          msgInfo.user_id === 1
+            ? {alignSelf: 'flex-end', backgroundColor: '#61B4CE', width: '50%'}
+            : {backgroundColor: '#8A50B8', width: '50%'}
+        }
+        key={msgInfo.description + i}>
         <Text style={{color: msgInfo.color}} key={msgInfo.description + i}>
           {msgInfo.name}
         </Text>
-        <Text key={msgInfo.description + i + i}>{msgInfo.description}</Text>
+        <Text key={msgInfo.description + i + i}>
+          {msgInfo.description}
+        </Text>
       </View>
     ))
+
     return (
-      <ScrollView>
+      <View style={{flex: 1}}>
+        <ScrollView
+          ref={ref => (this.scrollView = ref)}
+          onContentSizeChange={(contentWidth, contentHeight) => {
+            this.scrollView.scrollToEnd({animated: true})
+          }}
+          style={{flex: 1}}>
+          {chatMessages}
+        </ScrollView>
         <View style={styles.container}>
           <TextInput
-            style={{height: 40, borderWidth: 2}}
+            style={{height: 40, borderWidth: 2, bottom: 0, width: '100%'}}
             autoCorrect={false}
             value={this.props.chatMessage}
             onSubmitEditing={this.submitChatMessage}
             onChangeText={chatMessage => this.props.chatMessageInput(chatMessage)}
           />
-          {chatMessages}
         </View>
-      </ScrollView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#F5FCFF',
   },
 })
