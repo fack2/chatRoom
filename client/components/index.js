5763f0e0dbf818c0f9f5aa2d3c9a40d492ee8b07
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-11-19 21:58:29 +0200

    made signup style & add login link #2

--- client/components/index.js
+++ client/components/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
-import {Text} from 'react-native'
-import ChatRoom from './chatRoom'
-import SignUp from './SignUp'
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Text} from 'react-native';
+import ChatRoom from './chatRoom';
+import SignUp from './SignUp';
 
 const NavigationStack = createStackNavigator({
-  ChatRoom: {screen: ChatRoom},
   SignUp: {screen: SignUp},
-})
+  ChatRoom: {screen: ChatRoom},
+});
 
-const Routes = createAppContainer(NavigationStack)
+const Routes = createAppContainer(NavigationStack);
 
-export default Routes
+export default Routes;
