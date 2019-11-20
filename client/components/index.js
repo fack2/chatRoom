b2a292e97a160681ff760f8d7e731a4f984a6628
Sahar Adem <sahar.adem95@gmail.com>
2019-11-20 11:01:27 +0200

    #4 create login front end

--- client/components/index.js
+++ client/components/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
-import {Text} from 'react-native'
-import ChatRoom from './chatRoom'
-import SignUp from './SignUp'
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Text} from 'react-native';
+import ChatRoom from './chatRoom';
+import SignUp from './SignUp';
+import Login from './Login';
 
 const NavigationStack = createStackNavigator({
+  Login: {screen: Login},
   SignUp: {screen: SignUp},
   ChatRoom: {screen: ChatRoom},
-})
+});
 
-const Routes = createAppContainer(NavigationStack)
+const Routes = createAppContainer(NavigationStack);
 
-export default Routes
+export default Routes;
