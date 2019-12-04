5402b0dc5f0e00cd349106ffa58f96f09d67ed4f
YousefQwasmeh <151037@ppu.edu.ps>
2019-12-04 13:33:33 +0200

    logout button create logout button in chat room page

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,12 +5,13 @@ const { signup } = require("./signup");
 const validate = require("./validate");
 const { signupValidation } = require("./validation");
 const { login } = require("./login");
+const { logout } = require("./logout");
 const { auth } = require("../middlwares/auth");
 const { userInformation } = require("../controllers/userInformation");
 
 router.post("/signup", validate(signupValidation), signup);
 router.get("/getMessages", auth, getMessages);
-// router.get("/chech-auth", auth);
+router.get("/logout", logout);
 router.post("/login", login);
 router.get("/profile/:userId", userInformation);
 
